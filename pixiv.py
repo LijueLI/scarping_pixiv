2f17109bb59765f94d300c9f27958a4f66780b84
lijue-mac <boting87@gmail.com>
2019-08-05 01:54:57 +0800

    page can change but not flexible,record has bugs

--- pixiv.py
+++ pixiv.py
@@ -3,46 +3,59 @@ from bs4 import BeautifulSoup
 import os
 import urllib.request
 
+recordfile = open('record.txt','r')
+R = recordfile.readlines()
+
 driver = webdriver.Chrome(executable_path="chromedriver")
-driver.get('https://www.pixiv.net/search.php?s_mode=s_tag&word=kp31')
-pageSource = driver.page_source
-soup = BeautifulSoup(pageSource,'html5lib')
-sectiontag = soup.find(id="js-react-search-mid")
-a = sectiontag.findChildren("a")
-record = ""
-idlast = ""
-for href in a:
-    string = href.get('href')
-    id = string.partition('illust_id=')
-    if(id[2]!='' and id[2]!=idlast):
-        record = record+id[2]+'\n'
-        spans=href.findChildren("span")
-        index = 0
-        for span in spans:
-            if(span.text!=''):
-                index = int(span.text)
-        if(index != 0):
-            for i in range(1,index+1):
-                link = 'https://pixiv.cat/'+id[2]+'-'+str(i)+'.png'
+for page in range(1,18):
+    url = 'https://www.pixiv.net/search.php?word=kp31&order=date_d&p='+str(page)
+    driver.get(url)
+    pageSource = driver.page_source
+    soup = BeautifulSoup(pageSource,'html5lib')
+    sectiontag = soup.find(id="js-react-search-mid")
+    a = sectiontag.findChildren("a")
+    record = ""
+    idlast = ""
+    recordcount = 0
+    for href in a:
+        string = href.get('href')
+        id = string.partition('illust_id=')
+        if(id[2]!='' and id[2]!=idlast):
+            record = record+id[2]+'\n'
+            spans=href.findChildren("span")
+            index = 0
+            for span in spans:
+                if(span.text!=''):
+                    index = int(span.text)
+            if(index != 0):
+                for i in range(1,index+1):
+                    link = 'https://pixiv.cat/'+id[2]+'-'+str(i)+'.png'
+                    print(link)
+                    local = os.path.join('image/'+id[2]+'-'+str(i)+'.png')
+                    opener=urllib.request.build_opener()
+                    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36')]
+                    urllib.request.install_opener(opener)
+                    urllib.request.urlretrieve(link,local)
+            else:
+                link = 'https://pixiv.cat/'+id[2]+'.png'
                 print(link)
-                local = os.path.join('image/'+id[2]+'-'+str(i)+'.png')
+                local = os.path.join('image/'+id[2]+'.png')
                 opener=urllib.request.build_opener()
                 opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36')]
                 urllib.request.install_opener(opener)
                 urllib.request.urlretrieve(link,local)
-        else:
-            link = 'https://pixiv.cat/'+id[2]+'.png'
-            print(link)
-            local = os.path.join('image/'+id[2]+'.png')
-            opener=urllib.request.build_opener()
-            opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36')]
-            urllib.request.install_opener(opener)
-            urllib.request.urlretrieve(link,local)
-        #print(id[2])
-        idlast = id[2]
+            #print(id[2])
+            idlast = id[2]
+            for x in R:
+                if(id[2]+'\n'==x):
+                    recordcount=recordcount+1
+                    break
+            if(recordcount==3):
+                break
+recordfile.close()
 
-testfile = open('test.txt','w')
-testfile.write(record)
-testfile.close()
+recordfile = open('record.txt','w')
+recordfile.write(record)
+recordfile.close()
 
 driver.close()
